ca16e252ddea2a2ec64643ad4f42bbf70634226e
eb-cmoyano <cmoyano@eventbrite.com>
2019-05-02 16:01:31 -0300

    restrict filter

--- autotest/views.py
+++ autotest/views.py
@@ -9,19 +9,19 @@ class ResponseListView(ListView):
 
     def get_queryset(self):
         dni_val = self.request.GET.get('dni', '')
-        name_val = self.request.GET.get('name', '')
-        if dni_val or name_val:
+        new_context = Responses.objects.filter(dni=None)
+        if dni_val:
             new_context = Responses.objects.filter(
-                dni__contains=dni_val,
-                nombre_completo__contains=name_val,
+                dni=dni_val,
             )
-        else:
-            new_context = Responses.objects.all()
         return new_context
 
     def get_context_data(self, **kwargs):
         context = super(ResponseListView, self).get_context_data(**kwargs)
         context['dni'] = self.request.GET.get('dni', '')
-        context['name'] = self.request.GET.get('name', '')
-        context['autotest'] = Autotest.objects.all()[0]
+        autotest = Autotest.objects.all()
+        if autotest:
+            context['autotest'] = autotest[0]
+        else:
+            context['autotest'] = None
         return context
